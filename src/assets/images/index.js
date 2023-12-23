ac896e0c944c013d743e3446baf48d63743ffcc1
codetodev <saadiyafarook@gmail.com.com>
2023-12-23 16:18:18 +0530

    My first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './oie_201128463mDDAGS5.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo
+}
